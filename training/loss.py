e803ed4c44e8b856152aa970fd2b510c45c5d849
Tiankai Hang <seuhtk98@gmail.com>
2023-05-26 15:52:31 +0800

    Update loss.py

--- training/loss.py
+++ training/loss.py
@@ -102,4 +102,50 @@ class EDMLossMinSNR:
         D_yn = net(y + n, sigma, labels, augment_labels=augment_labels)
         loss = weight * ((D_yn - y) ** 2)
         return loss
+    
+@persistence.persistent_class
+class EDMLossMinSNR1:
+    def __init__(self, P_mean=-1.2, P_std=1.2, sigma_data=0.5):
+        self.P_mean = P_mean
+        self.P_std = P_std
+        self.sigma_data = sigma_data
+
+    def __call__(self, net, images, labels=None, augment_pipe=None):
+        rnd_normal = torch.randn([images.shape[0], 1, 1, 1], device=images.device)
+        sigma = (rnd_normal * self.P_std + self.P_mean).exp()
+        
+        snr = sigma ** 2
+        min_snr_k = snr.clone()
+        min_snr_k[min_snr_k >= 1] = 1
+        
+        weight = (sigma ** 2 + self.sigma_data ** 2) / (sigma * self.sigma_data) ** 2
+        weight = weight * (min_snr_k / snr)
+        y, augment_labels = augment_pipe(images) if augment_pipe is not None else (images, None)
+        n = torch.randn_like(y) * sigma
+        D_yn = net(y + n, sigma, labels, augment_labels=augment_labels)
+        loss = weight * ((D_yn - y) ** 2)
+        return loss
+    
+@persistence.persistent_class
+class EDMLossMinSNR10:
+    def __init__(self, P_mean=-1.2, P_std=1.2, sigma_data=0.5):
+        self.P_mean = P_mean
+        self.P_std = P_std
+        self.sigma_data = sigma_data
+
+    def __call__(self, net, images, labels=None, augment_pipe=None):
+        rnd_normal = torch.randn([images.shape[0], 1, 1, 1], device=images.device)
+        sigma = (rnd_normal * self.P_std + self.P_mean).exp()
+        
+        snr = sigma ** 2
+        min_snr_k = snr.clone()
+        min_snr_k[min_snr_k >= 10] = 10
+        
+        weight = (sigma ** 2 + self.sigma_data ** 2) / (sigma * self.sigma_data) ** 2
+        weight = weight * (min_snr_k / snr)
+        y, augment_labels = augment_pipe(images) if augment_pipe is not None else (images, None)
+        n = torch.randn_like(y) * sigma
+        D_yn = net(y + n, sigma, labels, augment_labels=augment_labels)
+        loss = weight * ((D_yn - y) ** 2)
+        return loss
 #----------------------------------------------------------------------------
